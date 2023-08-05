d6ad95efbbc2f7c201953b5417ae2b5c4c2a39ff
AZMAT ALI <222azmat@gmail.com>
2023-08-05 20:21:37 +0530

    submitting code-Sat Aug 05 2023 20:21:35 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,27 @@
 import React from "react";
 import './../styles/App.css';
 
+ // Assuming you have a CSS file to style the tooltip.
+
+const Tooltip = ({ text, children }) => {
+  return (
+    <div className="tooltip">
+      {children}
+      <span className="tooltiptext">{text}</span>
+    </div>
+  );
+};
+
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Tooltip Example</h1>
+      <Tooltip text="This is a tooltip text">
+        <span className="tooltip">Hover over me!</span>
+      </Tooltip>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+
